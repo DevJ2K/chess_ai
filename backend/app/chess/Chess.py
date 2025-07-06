6f58a9b47a00e896ed231b48a450480a8a2105b3
DevJ2K <ajavontheopro@gmail.com>
2025-07-07 00:29:38 +0200

    💭 : Start to test get_valid_moves method...

--- backend/app/chess/Chess.py
+++ backend/app/chess/Chess.py
@@ -1,5 +1,6 @@
 import numpy as np
-from app.chess.ChessTool import ChessTool
+import tests.configuration  # un-comment to ignore Numba JIT annotations
+from app.chess.ChessTool import get_valid_moves
 from app.utils.Colors import REDHB, RESET
 
 class Chess:
@@ -15,15 +16,16 @@ class Chess:
         board = np.zeros((8, 8), dtype=np.int8)
 
         # White pieces
-        board[1, :] = 1   # pawns
+        # board[1, :] = 1   # pawns
         board[0, 2] = board[0, 5] = 2   # bishops
         board[0, 1] = board[0, 6] = 3   # knights
         board[0, 0] = board[0, 7] = 4   # rooks
         board[0, 3] = 5   # queen
-        board[0, 4] = 6   # king
+        # board[0, 4] = 6   # king
+        board[1, 3] = 6   # king
 
         # Black pieces
-        board[6, :] = -1  # pawns
+        # board[6, :] = -1  # pawns
         board[7, 2] = board[7, 5] = -2  # bishops
         board[7, 1] = board[7, 6] = -3  # knights
         board[7, 0] = board[7, 7] = -4  # rooks
@@ -59,7 +61,7 @@ class Chess:
         pass
 
     def get_valid_moves(self) -> list[str]:
-        ChessTool.get_valid_moves(self.board, self.move_history)
+        get_valid_moves(self.board, len(self.move_history))
         # if len(self.move_history) % 2 == 0:  # White's turn
         #     pass
         # else:  # Black's turn
@@ -70,8 +72,18 @@ class Chess:
 
 if __name__ == "__main__":
     chess_game = Chess()
+    from benchmark.MeasureTime import MeasureTime
+    # from app.movements.piece_movements import warm_up_movement
+    from app.optimization.jit_configuration import warm_up_jit
+    # warm_up_movement()
+
+    # warm_up_jit()
+
     print(chess_game)
-    # chess_game.get_valid_moves()
+    measureTime = MeasureTime(start=True)
+    chess_game.get_valid_moves()
+    measureTime.stop()
+    print(chess_game)
 
     # example_move = "e4"
     # chess_game.apply_move(example_move)
