1b066fea6015a1086ee2f803140434aac2cb47a8
DevJ2K <ajavontheopro@gmail.com>
2025-07-09 19:27:54 +0200

    💭 : Add more functions in jit configuration...

--- backend/app/optimization/jit_configuration.py
+++ backend/app/optimization/jit_configuration.py
@@ -3,8 +3,7 @@ from time import time
 import numpy as np
 
 
-def warm_up_jit(
-        warmup_movement: bool = True):
+def warm_up_jit():
     # WARMUP Numba JIT compilation
     print(BHMAG + "Warming up Numba JIT compilation..." + RESET)
     start_time = time()
@@ -14,18 +13,16 @@ def warm_up_jit(
         get_pawn_moves, get_knight_moves, get_bishop_moves,
         get_rook_moves, get_queen_moves, get_king_moves)
 
-    if warmup_movement:
-        get_pawn_moves(np.zeros((8, 8), dtype=np.int8), 0, 0, True)
-        get_knight_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
-        get_bishop_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
-        get_rook_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
-        get_queen_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
-        get_king_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
+    get_pawn_moves(np.zeros((8, 8), dtype=np.int8), 0, 0, True)
+    get_knight_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
+    get_bishop_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
+    get_rook_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
+    get_queen_moves(np.zeros((8, 8), dtype=np.int8), 0, 0)
+    get_king_moves(np.zeros((8, 8), dtype=np.int8), 0, 0, False, False, False)
     # ====================================================
     #
     # ====================================================
-    # from app.movements.get_movements import is_not_dangerous_for_king, get_valid_moves
-    from app.movements.get_moves import is_not_dangerous_for_king, get_valid_moves
+    from app.movements.get_moves import is_not_dangerous_for_king, get_valid_moves, is_move_startswith
 
     get_valid_moves(
         np.zeros((8, 8), dtype=np.int8),
@@ -37,12 +34,15 @@ def warm_up_jit(
         np.array([0, 0], dtype=np.int8),
         is_white_turn=True
     )
+    is_move_startswith(
+        np.empty((0, 2, 3), dtype=np.int8),
+        np.array([[0, 0, 0], [0, 0, 0]], dtype=np.int8)
+    )
     # ====================================================
     #
     # ====================================================
-    from app.movements.king_movement_utils import is_king_in_check, is_square_under_attack, can_castle_kingside, can_castle_queenside
+    from app.movements.king_movement_utils import is_square_under_attack, can_castle_kingside, can_castle_queenside
 
-    is_king_in_check(np.zeros((8, 8), dtype=np.int8), 0, 0)
     is_square_under_attack(np.zeros((8, 8), dtype=np.int8), 0, 0, is_white_piece=True)
     can_castle_kingside(np.zeros((8, 8), dtype=np.int8), 0, 0, False)
     can_castle_queenside(np.zeros((8, 8), dtype=np.int8), 0, 0, False)
@@ -53,6 +53,15 @@ def warm_up_jit(
 
     is_in_board(np.zeros((8, 8), dtype=np.int8), 0, 0)
     # ====================================================
+    #
+    # ====================================================
+    from app.state.king import is_king_in_check
+    is_king_in_check(np.zeros((8, 8), dtype=np.int8), 0, 0)
+    # ====================================================
+    #
+    # ====================================================
+    from app.state.moves import has_available_moves
+    has_available_moves(np.zeros((8, 8), dtype=np.int8), np.empty((0, 2, 3), dtype=np.int8))
 
     end_time = time()
     print(BHGREEN + f"Numba JIT compilation warmed up in {end_time - start_time:.3f} seconds." + RESET)
