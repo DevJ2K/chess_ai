63282bf3e2ef9baad4212e2a132f6c221ffaa29c
DevJ2K <ajavontheopro@gmail.com>
2025-07-11 19:25:25 +0200

    ✅ : Create evaluate material function

--- backend/app/state/evaluate.py
+++ backend/app/state/evaluate.py
@@ -5,8 +5,47 @@ from app.state.moves import has_available_moves
 
 @njit
 def evaluate_board(board: np.ndarray, game_history: np.ndarray) -> int:
+    score = 0
+    score += evaluate_material(board) * 1.0
+    score += evaluate_mobility(board) * 0.1
+    score += evaluate_king_safety(board) * 0.5
+    score += evaluate_pawn_structure(board) * 0.3
+    score += evaluate_center_control(board) * 0.2
+    return score
 
-    is_white_turn = len(game_history) % 2 == 0
+@njit
+def evaluate_material(board: np.ndarray) -> int:
+    score = 0
+    piece_values = {
+        1: 1,   # Pawn
+        2: 3,   # Knight
+        3: 3,   # Bishop
+        4: 5,   # Rook
+        5: 9,   # Queen
+    }
+
+    for turn in [-1, 1]:
+        for piece_value, value in piece_values.items():
+            score += (np.count_nonzero(board == (piece_value * turn)) * value) * turn
+    return score
+
+# @njit
+def evaluate_mobility(board: np.ndarray) -> int:...
+
+# @njit
+def evaluate_king_safety(board: np.ndarray) -> int:...
+
+# @njit
+def evaluate_pawn_structure(board: np.ndarray) -> int:...
+
+# @njit
+def evaluate_center_control(board: np.ndarray) -> int:...
+
+
+# @njit
+# def _evaluate_board(board: np.ndarray, game_history: np.ndarray) -> int:
+
+#     is_white_turn = len(game_history) % 2 == 0
 
     if is_white_turn:
         pass
